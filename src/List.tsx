7856789f64e6453c6f2e95176a434de3bee0fe76
Huxulm <huxulm@gmail.com>
2024-04-08 13:24:39 +0800

    🎨 update DP list

--- src/List.tsx
+++ src/List.tsx
@@ -4,8 +4,7 @@ import ProblemCategory, { DP } from "./components/ProblemCatetory";
 export default function () {
     return <Container className="list">
         <div className="justify-content-center bg-white">
-            <ProblemCategory title="DP" data={DP} summary={DP[0].summary}/>
-            <p>整理自：<a className="link" href="https://leetcode.cn/circle/discuss/tXLS3i/">https://leetcode.cn/circle/discuss/tXLS3i/</a></p>
+            <ProblemCategory title={`<span>DP</span><p class="fs-6">整理自：<a target="_blank" className="link" href="https://leetcode.cn/circle/discuss/tXLS3i/">https://leetcode.cn/circle/discuss/tXLS3i/</a></p>`} data={DP} summary={DP[0].summary}/>
         </div>
     </Container >
 }
