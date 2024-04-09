38954a5af94e60bd09c436a2636cebb2908385cf
Huxulm <huxulm@gmail.com>
2024-04-09 21:53:51 +0800

    fix typo

--- src/List.tsx
+++ src/List.tsx
@@ -4,7 +4,7 @@ import ProblemCategory, { DP } from "./components/ProblemCatetory";
 export default function () {
     return <Container className="p-2">
         <div className="justify-content-center bg-white">
-            <ProblemCategory title={`<span>DP</span><p class="fs-6">整理自：<a target="_blank" className="link" href="https://leetcode.cn/circle/discuss/tXLS3i/">https://leetcode.cn/circle/discuss/tXLS3i/</a></p>`} data={DP} summary={DP[0].summary}/>
+            <ProblemCategory title={`<span>DP</span><p class="fs-6">整理自：<a target="_blank" class="link" href="https://leetcode.cn/circle/discuss/tXLS3i/">https://leetcode.cn/circle/discuss/tXLS3i/</a></p>`} data={DP} summary={DP[0].summary}/>
         </div>
     </Container >
 }
