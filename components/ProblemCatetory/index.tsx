cf3ca3fe7ea76e59b0c8c987a3783e13bbc3335f
Huxulm <huxulm@gmail.com>
2024-05-13 10:31:12 +0800

    remove draggable

--- components/ProblemCatetory/index.tsx
+++ components/ProblemCatetory/index.tsx
@@ -1,4 +1,3 @@
-import Draggable from "react-draggable"; // The default
 import RatingCircle, { ColorRating } from "../RatingCircle";
 
 type ProblemCategory = {
@@ -38,9 +37,9 @@ function count(data: ProblemCategory[]) {
   let tot = 0;
   for (let i = 0; i < data.length; i++) {
     if (!data[i].isLeaf) {
-      tot += count(data[i].child)
+      tot += count(data[i].child);
     } else {
-      tot += data[i].child? data[i].child.length:0;
+      tot += data[i].child ? data[i].child.length : 0;
     }
   }
   return tot;
@@ -72,9 +71,7 @@ function ProblemCategory({
             return (
               <div className={`pb-container level-${level + 2}`}>
                 {item.isLeaf ? (
-                  <Draggable>
-                    <ProblemCategoryList data={item} className={`leaf`} />
-                  </Draggable>
+                  <ProblemCategoryList data={item} className={`leaf`} />
                 ) : (
                   item.child &&
                   item.child?.map((item) => (
@@ -111,43 +108,41 @@ function ProblemCategoryList({
     return "col3";
   };
   return (
-    <Draggable>
-      <div className="shadow rounded p-2 leaf">
-        <h3 className="title" id={`${hashCode(data.title || "")}`}>
-          {data.title}
-        </h3>
-        {data.summary && (
-          <p
-            className="p-2 rounded summary bg-secondary-subtle text-warning-emphasis"
-            dangerouslySetInnerHTML={{ __html: data.summary }}
-          ></p>
-        )}
-        <ul className={`list p-2 ${data.child && getCols(data.child.length)}`}>
-          {data.child &&
-            data.child.map((item) => (
-              <li className="d-flex justify-content-between">
-                <a
-                  href={"https://leetcode.cn/problems" + item.src}
-                  target="_blank"
-                >
-                  {item.title}
-                </a>
-                {item.score && (
-                  <div className="ms-2 text-nowrap d-flex justify-content-center align-items-center pb-rating-bg">
-                    <RatingCircle difficulty={Number(item.score)} />
-                    <ColorRating
-                      className="rating-text"
-                      rating={Number(item.score)}
-                    >
-                      {Number(item.score).toFixed(0)}
-                    </ColorRating>
-                  </div>
-                )}
-              </li>
-            ))}
-        </ul>
-      </div>
-    </Draggable>
+    <div className="shadow rounded p-2 leaf">
+      <h3 className="title" id={`${hashCode(data.title || "")}`}>
+        {data.title}
+      </h3>
+      {data.summary && (
+        <p
+          className="p-2 rounded summary bg-secondary-subtle text-warning-emphasis"
+          dangerouslySetInnerHTML={{ __html: data.summary }}
+        ></p>
+      )}
+      <ul className={`list p-2 ${data.child && getCols(data.child.length)}`}>
+        {data.child &&
+          data.child.map((item) => (
+            <li className="d-flex justify-content-between">
+              <a
+                href={"https://leetcode.cn/problems" + item.src}
+                target="_blank"
+              >
+                {item.title}
+              </a>
+              {item.score && (
+                <div className="ms-2 text-nowrap d-flex justify-content-center align-items-center pb-rating-bg">
+                  <RatingCircle difficulty={Number(item.score)} />
+                  <ColorRating
+                    className="rating-text"
+                    rating={Number(item.score)}
+                  >
+                    {Number(item.score).toFixed(0)}
+                  </ColorRating>
+                </div>
+              )}
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 }
 
