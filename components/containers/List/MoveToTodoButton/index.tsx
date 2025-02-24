1725c4effa4e3fc639c1636c5da5ddde74c0c008
AutJ <autumnal_joy@foxmail.com>
2025-02-24 21:12:25 +0800

    feature: 题单页面跳转到todo题目按钮增加高亮效果

--- components/containers/List/MoveToTodoButton/index.tsx
+++ components/containers/List/MoveToTodoButton/index.tsx
@@ -1,5 +1,5 @@
 import { RandomIcon, TodoIcon } from "@components/icons";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 interface MoveToTodoButtonProps {
@@ -7,16 +7,29 @@ interface MoveToTodoButtonProps {
 }
 
 const MoveToTodoButton: React.FC<MoveToTodoButtonProps> = ({ random }) => {
+  const [isBlinking, setIsBlinking] = useState(false);
+
+  const handleBlink = (target: HTMLElement) => {
+    if (!isBlinking) {
+      target.classList.add("blinking-effect");
+      setTimeout(() => {
+        target.classList.remove("blinking-effect");
+        setIsBlinking(false);
+      }, 3000);
+      setIsBlinking(true);
+    }
+  };
+
   const scrollToTodo = () => {
     let targetElement: HTMLElement = null;
     if (random) {
       const todoElements = document.querySelectorAll(".zen-option-TODO");
       if (todoElements.length > 0) {
         const randomIndex = Math.floor(Math.random() * todoElements.length);
-        targetElement = todoElements[randomIndex] as HTMLElement;
+        targetElement = todoElements[randomIndex].closest("li") as HTMLElement;
       }
     } else {
-      targetElement = document.querySelector(".zen-option-TODO");
+      targetElement = document.querySelector(".zen-option-TODO").closest("li");
     }
     if (targetElement) {
       const yOffset = window.innerHeight / 2;
@@ -26,6 +39,7 @@ const MoveToTodoButton: React.FC<MoveToTodoButtonProps> = ({ random }) => {
         left: 0,
         behavior: "smooth",
       });
+      handleBlink(targetElement);
     }
   };
 
