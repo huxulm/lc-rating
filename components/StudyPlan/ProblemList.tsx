ced857ab45df42ac0106591c77b96c04ba6a2b98
AutJ <autumnal_joy@foxmail.com>
2025-03-11 23:11:44 +0800

    optim: 补充题单页面题目超链接

--- components/StudyPlan/ProblemList.tsx
+++ components/StudyPlan/ProblemList.tsx
@@ -2,13 +2,19 @@ import { ProgressSelector } from "@/components/common/ProgressSelector";
 import { RatingCircle, ratingInfo } from "@/components/common/RatingCircle";
 import { Separator } from "@/components/ui/separator";
 import { StudyPlanData } from "@/types";
+import Link from "next/link";
 import React from "react";
+import { LC_HOST_ZH, LC_HOST_EN } from "@/config/constants";
+import { useGlobalSettingsStore } from "@/hooks/useGlobalSettings";
 
 interface ProblemListProps {
   problems: StudyPlanData.Item[];
 }
 
 const ProblemList = React.memo(({ problems }: ProblemListProps) => {
+  const { language } = useGlobalSettingsStore();
+  const LC_HOST = language === "zh" ? LC_HOST_ZH : LC_HOST_EN;
+
   return (
     <div className="flex flex-col">
       {problems.map((problem) => {
@@ -18,7 +24,13 @@ const ProblemList = React.memo(({ problems }: ProblemListProps) => {
           <div key={problem.title}>
             <Separator />
             <div className="flex flex-row items-center text-pretty justify-between p-1">
-              <span>{problem.title}</span>
+              <Link
+                href={`${LC_HOST}/problems/${problem.slug}`}
+                target="_blank"
+                className="hover:underline"
+              >
+                {problem.title}
+              </Link>
               <div className="flex flex-row items-center gap-2">
                 {problem.score ? (
                   <div className="flex flex-row items-center">
