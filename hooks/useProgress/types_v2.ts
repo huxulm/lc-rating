a65221edac2fc6f4aaeb807d7b35aef20c0cb8bc
AutJ <autumnal_joy@foxmail.com>
2025-03-29 21:59:37 +0800

    fix: 缓存的依赖缺失造成数据错误

--- hooks/useProgress/types_v2.ts
+++ hooks/useProgress/types_v2.ts
@@ -7,10 +7,10 @@ export interface ProgressStoreState {
 }
 
 interface ProgressStoreActions {
+  getProgress: (id: string) => OptionKey | undefined;
   setProgress: (id: string, value: OptionKey) => void;
   delProgress: (id: string) => void;
   setAllProgress: (progress: Record<string, OptionKey>) => void;
-  getProgress: (id: string) => OptionKey | undefined;
 }
 
 export type ProgressStore = ProgressStoreState & ProgressStoreActions;
